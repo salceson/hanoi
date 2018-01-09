6848bb6cee145b49e14bb1aa424038ef60c2ea4b
Michal Ciolczyk <michal.ciolczyk@evidenceprime.com>
2018-01-09 18:01:54 +0100

    Displaying time between first move and finish

--- app/components/game.js
+++ app/components/game.js
@@ -4,7 +4,7 @@ import { defaultBlocksNum } from '../config';
 import { generateDefaultStick, moveBlock } from '../utils';
 
 import GameBoard from './gameBoard';
-import SettingsAndMovesDisplay from './settingsAndMovesDisplay';
+import GameControlPanel from './gameControlPanel';
 
 import style from '../styles/game.css';
 
@@ -15,12 +15,28 @@ class Game extends React.Component {
       blocksNum: defaultBlocksNum,
       sticks: [generateDefaultStick(defaultBlocksNum), [], []],
       won: false,
+      isTimerGoing: false,
       moves: 0,
+      time: 0,
+      interval: null,
     };
     this.changeBlocksNum = this.changeBlocksNum.bind(this);
     this.reset = this.reset.bind(this);
     this.canMove = this.canMove.bind(this);
     this.move = this.move.bind(this);
+    this.updateTime = this.updateTime.bind(this);
+  }
+
+  componentWillMount() {
+    this.setState({
+      interval: setInterval(this.updateTime, 1000),
+    });
+  }
+
+  componentWillUnmount() {
+    const { interval } = this.state;
+    clearInterval(interval);
+    this.setState({ interval: null });
   }
 
   changeBlocksNum(blocksNum) {
@@ -29,6 +45,8 @@ class Game extends React.Component {
       sticks: [generateDefaultStick(blocksNum), [], []],
       won: false,
       moves: 0,
+      isTimerGoing: false,
+      time: 0,
     });
   }
 
@@ -37,7 +55,16 @@ class Game extends React.Component {
       sticks: [generateDefaultStick(this.state.blocksNum), [], []],
       won: false,
       moves: 0,
+      isTimerGoing: false,
+      time: 0,
     });
+  }
+
+  updateTime() {
+    const { isTimerGoing, time } = this.state;
+    if (isTimerGoing) {
+      this.setState({ time: time + 1 });
+    }
   }
 
   canMove(block, toStickIdx) {
@@ -50,15 +77,20 @@ class Game extends React.Component {
       return;
     }
     const newSticks = moveBlock(this.state.sticks, fromStickIdx, toStickIdx);
+
+    const won = newSticks[2].length === this.state.blocksNum;
     this.setState({
+      isTimerGoing: !won,
       sticks: newSticks,
-      won: newSticks[2].length === this.state.blocksNum,
+      won,
       moves: this.state.moves + 1,
     });
   }
 
   render() {
-    const { blocksNum, sticks, moves } = this.state;
+    const {
+      blocksNum, sticks, moves, time,
+    } = this.state;
     return (
       <div className={style.game}>
         <div className={style.title}>Wieże Hanoi</div>
@@ -66,12 +98,13 @@ class Game extends React.Component {
           <p>Przenieś wszystkie krążki z lewego słupka na prawy.</p>
           <p>Możesz upuścić tylko mniejszy klocek na większy lub dowolny klocek na pusty słupek.</p>
         </div>
-        <SettingsAndMovesDisplay
+        <GameControlPanel
           onChangeBlocksNum={this.changeBlocksNum}
           onReset={this.reset}
           blocksNum={blocksNum}
           moves={moves}
           won={this.state.won}
+          time={time}
         />
         <GameBoard
           blocksNum={blocksNum}
--- app/components/gameControlPanel.js
+++ app/components/gameControlPanel.js
@@ -3,15 +3,16 @@ import PropTypes from 'prop-types';
 
 import { minBlocksNum, maxBlocksNum } from '../config';
 
-import style from '../styles/settingsAndMovesDisplay.css';
+import style from '../styles/gameControlPanel.css';
 
-class SettingsAndMovesDisplay extends React.Component {
+class GameControlPanel extends React.Component {
   static propTypes = {
     onChangeBlocksNum: PropTypes.func.isRequired,
     onReset: PropTypes.func.isRequired,
     blocksNum: PropTypes.number.isRequired,
     moves: PropTypes.number.isRequired,
     won: PropTypes.bool.isRequired,
+    time: PropTypes.number.isRequired,
   };
 
   constructor() {
@@ -31,6 +32,17 @@ class SettingsAndMovesDisplay extends React.Component {
         Liczba ruchów: {moves}
         {won && ` (minimalna: ${minMoves})`}
       </span>
+    );
+  }
+
+  renderTime() {
+    const { time } = this.props;
+    const s = time % 60;
+    const seconds = (s < 10) ? `0${s}` : s;
+    const m = (time - s) / 60;
+    const minutes = (m < 10) ? `0${m}` : m;
+    return (
+      <span className={style.time}>Czas: {minutes}:{seconds}</span>
     );
   }
 
@@ -62,9 +74,10 @@ class SettingsAndMovesDisplay extends React.Component {
         {this.renderSelect()}
         <button onClick={onReset}>Od nowa</button>
         {this.renderMoves()}
+        {this.renderTime()}
       </div>
     );
   }
 }
 
-export default SettingsAndMovesDisplay;
+export default GameControlPanel;
